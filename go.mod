b1df9dffb489e33e5f5780595dd98cb3e8b838a9
Florin Pățan <florinpatan@gmail.com>
2019-04-28 15:19:12 +0800

    Version bump

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dlsniper/gomoduleslibrary
+module github.com/dlsniper/gomoduleslibrary/v2
 
 go 1.12
--- messages/file.go
+++ messages/file.go
@@ -6,7 +6,10 @@ func NewHello() *Hello {
 	return &Hello{}
 }
 
-func (Hello) Say() string {
+func (Hello) Say(lang string) string {
 	message := "你好，北京！"
+	if lang == "ro" {
+		message = "Salut Beijing!"
+	}
 	return message
 }
